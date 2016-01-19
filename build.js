17ed8ed0993fa06cf5f7a0d6354100ed9d2a3cf3
YuuKohaku <heimaolee@gmail.com>
2016-01-20 00:28:10 +0600

    package update

--- build.js
+++ build.js
@@ -4,24 +4,17 @@ let gulp = require("gulp");
 let babel = require("gulp-babel");
 
 gulp.task("es6-js", function() {
-  return gulp.src(["src/**/*.js", "tests/**/*.js"])
-    .pipe(babel({
-      "whitelist": [
-        "strict",
-        "es6.modules",
-        "es6.parameters",
-        "es6.destructuring"
-      ]
-    }))
-    .pipe(gulp.dest("build"))
-    .on('end', function() {
-      console.log('end build');
-    });
+	return gulp.src(["src/**/*.js", "tests/**/*.js"])
+		.pipe(babel())
+		.pipe(gulp.dest("build"))
+		.on('end', function() {
+			console.log('end build');
+		});
 });
 
 gulp.task("json", function() {
-  return gulp.src(["src/**/*.json"])
-    .pipe(gulp.dest("build"));
+	return gulp.src(["src/**/*.json"])
+		.pipe(gulp.dest("build"));
 });
 
 gulp.task('default', ['es6-js', 'json']);
