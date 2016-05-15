76d1e4930804a351e1a0d8525dbe943fbd78dac0
artificial-life <proteus.protocol@gmail.com>
2016-05-15 16:28:08 +0600

    event-queue adapter

--- src/event-queue/event-queue.js
+++ src/event-queue/event-queue.js
@@ -1,36 +1,38 @@
 'use strict'
 
 var Queue = function (name, options) {
-    var local_subs = {};
-    var adapters = [];
-    return {
-        _on: function (event_name, cb) {
-            if (!local_subs.hasOwnProperty(event_name)) local_subs[event_name] = [];
-            local_subs[event_name].push(cb);
-            // console.log(process.pid, ':', local_subs);
-        },
-        on: function (event_name, cb) {
-            this._on(event_name, cb);
-            var i;
-            for (i = 0; i < adapters.length; i += 1) {
-                adapters[i].sendLinkRequest(event_name);
-            }
-        },
-        emit: function (event_name, data) {
-            var subs = local_subs[event_name];
-            if (!subs) return;
-            var i,
-                len = subs.length;
+	let local_subs = {};
+	let adapter = false;
+	return {
+		_on: function (event_name, cb) {
+			if (!local_subs.hasOwnProperty(event_name)) local_subs[event_name] = [];
+			local_subs[event_name].push(cb);
+		},
+		on: function (event_name, cb) {
+			this._on(event_name, cb);
 
-            for (i = 0; i < len; i += 1) {
-                subs[i].call(null, data);
-            }
-        },
-        addAdapter: function (adapter) {
-            adapters.push(adapter);
-            adapter.attach(this);
-        }
-    }
+			adapter && adapter.subscribe(event_name);
+		},
+		emit: function (event_name, data) {
+			adapter && adapter.emit(event_name, data);
+
+			this._emit(event_name, data)
+		},
+		_emit: function (event_name, data) {
+			let subs = local_subs[event_name];
+			if (!subs) return;
+			let i;
+			let len = subs.length;
+
+			for (i = 0; i < len; i += 1) {
+				subs[i].call(null, data);
+			}
+		},
+		addAdapter: function (pubsub) {
+			adapter = pubsub;
+			adapter.onMessage((event_name, data) => this._emit(event_name, data))
+		}
+	}
 };
 
 module.exports = Queue;
